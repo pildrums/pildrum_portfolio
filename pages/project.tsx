6b86286c2ca6b93ca196f5797de3cd5d5e015d19
pildrums <there7788@gmail.com>
2023-01-06 08:39:45 +0900

    Complete project item image insert

--- pages/project.tsx
+++ pages/project.tsx
@@ -1,6 +1,7 @@
 import Layout from "Components/Layout";
 import Seo from "Components/Seo";
 import { motion } from "framer-motion";
+import Image from "next/image";
 import Link from "next/link";
 import styled from "styled-components";
 
@@ -13,9 +14,9 @@ const INFO = [
     git: "https://github.com/pildrums/mcDonalds",
     url: "https://pildrums.github.io/mcDonalds/",
     description:
-      "리액트의 심화과정을 공부하고 처음 직접 작업한 맥도날드 랜딩페이지입니다. 이 프로젝트를 통해서 컴포넌트 리팩토링의 중요성을 배웠습니다. 리액트를 제대로 배우기 위해서 최대한 라이브러리의 도움없이 오직 구글링과 저의 프로그래밍 지식으로만 만들어습니다. 기존의 작업방식의 문제점들을 고칠 수 있었던 소중한 프로젝트입니다.",
-    periods: "2021.12.25 ~ 2022.02.05",
-    stack: "React(JS) / Styled-Components",
+      " 리액트의 심화과정을 공부하고 처음 직접 작업한 맥도날드 랜딩페이지입니다. 이 프로젝트를 통해서 컴포넌트 리팩토링의 중요성을 배웠습니다. 리액트를 제대로 배우기 위해서 최대한 라이브러리의 도움없이 오직 구글링과 저의 리액트 기본 지식으로만 만들어습니다. 기존의 작업방식의 문제점들을 고칠 수 있었던 소중한 프로젝트입니다. 추후에 UI 개선 및 다른 페이지의 작업도 기획 중입니다.",
+    periods: " 2021.12.25 ~ 2022.02.05",
+    stack: " React(JS) / Styled-Components",
     img: "assets/",
   },
   {
@@ -24,9 +25,9 @@ const INFO = [
     git: "https://github.com/pildrums/Pilflix",
     url: "https://pildrums.github.io/Pilflix/",
     description:
-      "노마드코더의 React-MasterClass 강의를 보면서 타입스크립트로 구성한 프로젝트입니다. 강의에 과제로 남겨둔 부분은 구글링을 통해서 직접 구현했습니다. 현재 UI와 상태관리 라이브러리를 Recoil로 교체를 위해 다시 기획 중입니다.",
+      "노마드코더의 React-MasterClass 강의를 보면서 타입스크립트로 구성한 프로젝트입니다. 강의에 과제로 남겨둔 시리즈 페이지와 검색기능은 구글링을 통해서 직접 구현했습니다. 현재 UI와 상태관리 라이브러리를 Recoil로 교체를 위해 다시 기획 중입니다.",
     periods: "2022.12.13 ~ 2022.12.29",
-    stack: "React(TS) / Styled-Components / Framer-Motion",
+    stack: "React(TS) / Styled-Components / Framer-Motion / React-Query",
     img: "assets/pilflix.png",
   },
 ];
@@ -46,15 +47,17 @@ export default function Project() {
               key={item.id}
             >
               <ItemTitle>{item.title}</ItemTitle>
-              <ItemImage>
+              <ItemImage bgimage={item.img}>
                 <ItemContainer>
-                  <span>{item.description}</span>
-                  <span>제작기간: {item.periods}</span>
-                  <span>{item.stack}</span>
-                  <ItemButtonContainer>
-                    <ItemButton href={item.url}>바로가기</ItemButton>
-                    <ItemButton href={item.git}>Github</ItemButton>
-                  </ItemButtonContainer>
+                  <Overlay>
+                    <span>• {item.description}</span>
+                    <span>• 제작기간: {item.periods}</span>
+                    <span>• {item.stack}</span>
+                    <ItemButtonContainer>
+                      <ItemButton href={item.url}>바로가기</ItemButton>
+                      <ItemButton href={item.git}>Github</ItemButton>
+                    </ItemButtonContainer>
+                  </Overlay>
                 </ItemContainer>
               </ItemImage>
             </Item>
@@ -115,32 +118,46 @@ const ItemTitle = styled.h2`
   font-weight: 400;
 `;
 
-const ItemImage = styled.div`
+const ItemImage = styled.div<{ bgimage: string }>`
   width: 600px;
   height: 450px;
-  background: coral;
+  background: coral; /* default background */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  border-radius: 10px;
+  background-image: url(${(props) => props.bgimage});
+  background-size: cover;
+`;
+
+const Overlay = styled.div`
+  width: inherit;
+  height: inherit;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+  padding: 50px;
   border-radius: 10px;
 `;
 
 const ItemContainer = styled.div`
   width: inherit;
   height: inherit;
-  padding: 0 50px;
   opacity: 0;
   transition: opacity 0.3s ease-in-out;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 20px;
+
   &:hover {
     opacity: 1;
   }
   span {
     font-size: 16px;
+    color: ${(props) => props.theme.white.lighter};
+    &:first-child {
+      line-height: 24px;
+    }
   }
 `;
 
@@ -148,6 +165,7 @@ const ItemButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   gap: 20px;
+  margin-top: 20px;
 `;
 
 const ItemButton = styled(Link)`
