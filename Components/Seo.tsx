289f9d828bd88123414615139b7366c5a740b9aa
pildrums <there7788@gmail.com>
2023-01-09 05:56:08 +0900

    Add favicon & Deploy project

--- Components/Seo.tsx
+++ Components/Seo.tsx
@@ -8,6 +8,7 @@ export default function Seo({ title }: ISeoProps) {
   return (
     <Head>
       <title>{title} | 김필진 포트폴리오</title>
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     </Head>
   );
 }
